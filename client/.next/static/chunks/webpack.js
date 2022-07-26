bc740439c50f777a8e2a958839d5ae51434ba9e4
zizpzn <zizpzn@gmail.com>
2022-07-26 17:10:48 +0700

    AWS SES

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "32e7a9a6da677800"; }
+/******/ 		__webpack_require__.h = function() { return "071fb3963abbc661"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
