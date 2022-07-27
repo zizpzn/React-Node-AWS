17ff53cf12c1873909518672ee7109ee7c684c48
zizpzn <zizpzn@gmail.com>
2022-07-27 10:34:41 +0700

    success, error message

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "071fb3963abbc661"; }
+/******/ 		__webpack_require__.h = function() { return "3b579c00053c3810"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Layout from "../components/Layout";
 import axios from "axios";
+import { showSuccessMessage, showErrorMessage } from "../helpers/alerts";
 
 const Register = () => {
   const [state, setState] = useState({
-    name: "",
-    email: "",
-    password: "",
+    name: "Ngan",
+    email: "zizpzn@gmail.com",
+    password: "1234",
     error: "",
     success: "",
     buttonText: "Register",
@@ -26,6 +27,7 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setState({ ...state, buttonText: "Registering" });
     // console.table({ name, email, password });
     axios
       .post(`http://localhost:8000/api/register`, {
@@ -33,8 +35,25 @@ const Register = () => {
         email,
         password,
       })
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+      .then((response) => {
+        console.log(response);
+        setState({
+          ...state,
+          name: "",
+          email: "",
+          password: "",
+          buttonText: "Submitted",
+          success: response.data.message,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setState({
+          ...state,
+          buttonText: "Register",
+          error: error.response.data.error,
+        });
+      });
   };
 
   const registerForm = () => (
@@ -77,9 +96,9 @@ const Register = () => {
       <div className="col-md-6 offset-md-3">
         <h1>Register</h1>
         <br />
+        {success && showSuccessMessage(success)}
+        {error && showErrorMessage(error)}
         {registerForm()}
-        <hr />
-        {JSON.stringify(state)}
       </div>
     </Layout>
   );
